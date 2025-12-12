361209cfb775aeac6fa3979c9e770af598cbeec4
wxt <wb.wangxiaoteng01@mesg.corp.netease.com>
2025-12-12 14:59:04 +0800

    update:path

--- js/load.js
+++ js/load.js
@@ -17,8 +17,8 @@ Game.Load.prototype = {
 	    label = game.add.text(w / 2, h / 2, "loading...", { fill: "#fff", font: "30px Arial" });
 		label.anchor.setTo(0.5, 0.5);
 
-		game.load.spritesheet("player", "public/player.png", 20, 24);
-		game.load.spritesheet("enemy1", "public/enemy1.png", 32, 36);
-		game.load.spritesheet("enemy2", "public/enemy2.png", 28, 40);
+		game.load.spritesheet("player", "./public/player.png", 20, 24);
+		game.load.spritesheet("enemy1", "./public/enemy1.png", 32, 36);
+		game.load.spritesheet("enemy2", "./public/enemy2.png", 28, 40);
 	}
 };
